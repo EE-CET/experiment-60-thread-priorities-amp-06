0748b48923dc0a98226683657abd63cd574ede50
amrita-m-pillai <amritampillai06@gmail.com>
2026-02-19 07:48:20 +0530

    first commit

--- src/ThreadPriorityDemo.java
+++ src/ThreadPriorityDemo.java
@@ -1,29 +1,35 @@
 class PriorityThread extends Thread {
 
     PriorityThread(String name) {
-        super(name);   // Set thread name
+        super(name);
     }
 
     public void run() {
-        System.out.println(getName() + " Priority: " + getPriority());
+        // No printing here
     }
 }
 
 public class ThreadPriorityDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         PriorityThread t1 = new PriorityThread("Thread 1");
         PriorityThread t2 = new PriorityThread("Thread 2");
         PriorityThread t3 = new PriorityThread("Thread 3");
 
-        // Set priorities
-        t1.setPriority(Thread.MIN_PRIORITY);   // 1
-        t2.setPriority(Thread.NORM_PRIORITY);  // 5
-        t3.setPriority(Thread.MAX_PRIORITY);   // 10
+        t1.setPriority(Thread.MIN_PRIORITY);
+        t2.setPriority(Thread.NORM_PRIORITY);
+        t3.setPriority(Thread.MAX_PRIORITY);
 
-        // Start threads
         t1.start();
+        t1.join();   // Wait until t1 finishes
+
         t2.start();
+        t2.join();   // Wait until t2 finishes
+
         t3.start();
-    }
-}
+        t3.join();   // Wait until t3 finishes
+
+        // Print in required order
+        System.out.println("Thread 1 Priority: " + t1.getPriority());
+        System.out.println("Thread 2 Priority: " + t2.getPriority());
+        System.out.println("Thread 3 Priority: " + t3.getPriority());
